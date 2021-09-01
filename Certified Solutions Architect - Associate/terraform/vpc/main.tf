6005d91b531db988c38e8a3c7f313fb0f941c8d8
Devin Miller <dmiller@Devins-MacBook-Air.local>
2021-08-31 20:01:48 -0700

    Working...

--- Certified Solutions Architect - Associate/terraform/vpc/main.tf
+++ Certified Solutions Architect - Associate/terraform/vpc/main.tf
@@ -1,6 +1,9 @@
 terraform {
   backend "s3" {
-    
+    bucket          = "cotb.terraform"
+    key             = "aws-training/vpc/tf.tfstate"
+    dynamodb_table  = "aws_cotb_dev_terraform_state"
+    region          = "us-west-2"
   }
   required_providers {
     aws = {
@@ -13,4 +16,10 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
+}
+
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+resource "aws_vpc" "cotb_dev_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
