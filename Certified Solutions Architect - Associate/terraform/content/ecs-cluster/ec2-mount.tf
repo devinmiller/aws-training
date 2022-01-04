d7230c7f380ad6c90abcef80d6eb69e4f1d4d1d6
Devin Miller <devin.miller@usfoods.com>
2022-01-04 16:38:34 -0700

    Working on ecs cluster tasks

--- Certified Solutions Architect - Associate/terraform/content/ecs-cluster/ec2-mount.tf
+++ Certified Solutions Architect - Associate/terraform/content/ecs-cluster/ec2-mount.tf
@@ -1,62 +1,62 @@
-data "aws_ami" "amzn2_ami" {
-  most_recent = true
+# data "aws_ami" "amzn2_ami" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-kernel-*"]
+#   }
 
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
+#   filter {
+#     name   = "architecture"
+#     values = ["x86_64"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["137112412989"] # Amazon
-}
+#   owners = ["137112412989"] # Amazon
+# }
 
-resource "aws_instance" "cotb_mount_instance" {
-  depends_on                  = [aws_efs_file_system.cotb_cluster_file_system]
-  # AMI to use for the instance.
-  ami                         = data.aws_ami.amzn2_ami.id
-  # Whether to associate a public IP address with an instance in a VPC
-  associate_public_ip_address = true
-  # If true, enables EC2 Instance Termination Protection (defaults to false)
-  disable_api_termination     = false 
-  #  If true, the launched EC2 instance will support hibernation
-  hibernation                 = false
-  # The instance type to use for the instance
-  instance_type               = "t3a.micro"
-  # Key name of the Key Pair to use for the instance
-  key_name                    = var.ssh_key
-  # VPC Subnet ID to launch in
-  subnet_id = aws_subnet.cotb_cluster_public_subnets[0].id
-  # A list of security group IDs to associate with
-  vpc_security_group_ids = [ 
-    aws_security_group.cotb_cluster_ssh_sg.id,
-    aws_security_group.cotb_cluster_nfs_sg.id
-  ]
+# resource "aws_instance" "cotb_mount_instance" {
+#   depends_on                  = [aws_efs_file_system.cotb_cluster_file_system]
+#   # AMI to use for the instance.
+#   ami                         = data.aws_ami.amzn2_ami.id
+#   # Whether to associate a public IP address with an instance in a VPC
+#   associate_public_ip_address = true
+#   # If true, enables EC2 Instance Termination Protection (defaults to false)
+#   disable_api_termination     = false 
+#   #  If true, the launched EC2 instance will support hibernation
+#   hibernation                 = false
+#   # The instance type to use for the instance
+#   instance_type               = "t3a.micro"
+#   # Key name of the Key Pair to use for the instance
+#   key_name                    = var.ssh_key
+#   # VPC Subnet ID to launch in
+#   subnet_id = aws_subnet.cotb_cluster_public_subnets[0].id
+#   # A list of security group IDs to associate with
+#   vpc_security_group_ids = [ 
+#     aws_security_group.cotb_cluster_ssh_sg.id,
+#     aws_security_group.cotb_cluster_nfs_sg.id
+#   ]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum install amazon-efs-utils
-    sudo mkdir /efs
-    sudo mount -t efs ${aws_efs_file_system.cotb_cluster_file_system.id}:/ /efs
-    EOF
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     sudo yum install amazon-efs-utils
+#     sudo mkdir /efs
+#     sudo mount -t efs ${aws_efs_file_system.cotb_cluster_file_system.id}:/ /efs
+#     EOF
 
-  root_block_device {
-    # Whether the volume should be destroyed on instance termination
-    # Defaults to true for the root volume
-    delete_on_termination = true
-    # Size of the volume in gibibytes
-    volume_size = "30"
-  }
+#   root_block_device {
+#     # Whether the volume should be destroyed on instance termination
+#     # Defaults to true for the root volume
+#     delete_on_termination = true
+#     # Size of the volume in gibibytes
+#     volume_size = "30"
+#   }
 
-  tags = {
-    Name = "cotb-mount-instance"
-  }
-}
+#   tags = {
+#     Name = "cotb-mount-instance"
+#   }
+# }
--- Certified Solutions Architect - Associate/terraform/content/ecs-cluster/ecs.tf
+++ Certified Solutions Architect - Associate/terraform/content/ecs-cluster/ecs.tf
@@ -1,7 +1,7 @@
-# # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_cluster
-# resource "aws_ecs_cluster" "cotb_dev_cluster" {
-#   name = "cotb-dev-cluster"
-# } 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_cluster
+resource "aws_ecs_cluster" "cotb_dev_cluster" {
+  name = "cotb-dev-cluster"
+} 
 
 # resource "aws_ecs_task_definition" "sample_task_one" {
 #   # A unique name for your task definition
@@ -9,10 +9,7 @@
 
 #   requires_compatibilities  = ["EC2"]
 #   # Docker networking mode to use for the containers in the task.
-#   network_mode              = "awsvpc"
-
-#   deployment_maximum_percent = 100
-
+#   # network_mode              = "awsvpc"
 
 #   # A list of valid container definitions provided as a single valid JSON document
 #   container_definitions = jsonencode([
@@ -24,8 +21,14 @@
 #       essential = true
 #       portMappings = [
 #         {
-#           containerPort = 80
-#           hostPort      = 80
+#           containerPort = 8080
+#           hostPort = 8080
+#         }
+#       ]
+#       environment = [
+#         {
+#           "name" = "NGINX_PORT"
+#           "value" = "8080"
 #         }
 #       ]
 #       healthCheck = {
@@ -55,23 +58,23 @@
 #   launch_type     = "EC2"
 
 #   # Network configuration for the service. 
-#   network_configuration {
-#     # Subnets associated with the task or service.
-#     subnets = [for subnet in aws_subnet.cotb_cluster_public_subnets : subnet.id]
-#     # Security groups associated with the task or service.
-#     security_groups    = [aws_security_group.cotb_cluster_public_sg.id]
-#     # Assign a public IP address to the ENI.
-#     assign_public_ip = false
-#   }
+#   # network_configuration {
+#   #   # Subnets associated with the task or service.
+#   #   subnets = [for subnet in aws_subnet.cotb_cluster_public_subnets : subnet.id]
+#   #   # Security groups associated with the task or service.
+#   #   security_groups    = [aws_security_group.cotb_cluster_public_sg.id]
+#   #   # Assign a public IP address to the ENI.
+#   #   assign_public_ip = false
+#   # }
 
 #   # Service discovery registries for the service. 
 #   service_registries {
 #     # ARN of the Service Registry.
-#     registry_arn = aws_service_discovery_service.cotb_cluster_sd_service.arn
-#     # Port value, already specified in the task definition, to be used for your service discovery service.
-#     # container_port = 80
-#     # Container name value, already specified in the task definition, to be used for your service discovery service.
-#     # container_name = "sample-task-one"
+#     registry_arn = aws_service_discovery_service.cotb_cluster_sd_service_one.arn
+#     # Port value
+#     container_port = 8080
+#     # Container name value
+#     container_name = "sample-task-one"
 #   }
 
 #   tags = {
@@ -87,7 +90,8 @@
 
 #   requires_compatibilities  = ["EC2"]
 #   # Docker networking mode to use for the containers in the task.
-#   network_mode              = "awsvpc"
+#   # network_mode              = "awsvpc"
+
 #   # A list of valid container definitions provided as a single valid JSON document
 #   container_definitions = jsonencode([
 #     {
@@ -98,8 +102,14 @@
 #       essential = true
 #       portMappings = [
 #         {
-#           containerPort = 80
-#           hostPort      = 80
+#           containerPort = 8081
+#           hostPort = 8081
+#         }
+#       ]
+#       environment = [
+#         {
+#           "name" = "NGINX_PORT"
+#           "value" = "8081"
 #         }
 #       ]
 #       healthCheck = {
@@ -129,28 +139,109 @@
 #   launch_type     = "EC2"
 
 #   # Network configuration for the service. 
-#   network_configuration {
-#     # Subnets associated with the task or service.
-#     subnets = [for subnet in aws_subnet.cotb_cluster_public_subnets : subnet.id]
-#     # Security groups associated with the task or service.
-#     security_groups    = [aws_security_group.cotb_cluster_public_sg.id]
-#     # Assign a public IP address to the ENI.
-#     assign_public_ip = false
-#   }
+#   # network_configuration {
+#   #   # Subnets associated with the task or service.
+#   #   subnets = [for subnet in aws_subnet.cotb_cluster_public_subnets : subnet.id]
+#   #   # Security groups associated with the task or service.
+#   #   security_groups    = [aws_security_group.cotb_cluster_public_sg.id]
+#   #   # Assign a public IP address to the ENI.
+#   #   assign_public_ip = false
+#   # }
 
 #   # Service discovery registries for the service. 
 #   service_registries {
 #     # ARN of the Service Registry.
-#     registry_arn = aws_service_discovery_service.cotb_cluster_sd_service.arn
-#     # Port value, already specified in the task definition, to be used for your service discovery service.
-#     # container_port = 80
-#     # Container name value, already specified in the task definition, to be used for your service discovery service.
-#     # container_name = "sample-task-one"
+#     registry_arn = aws_service_discovery_service.cotb_cluster_sd_service_two.arn
+#     # Port value
+#     container_port = 8081
+#     # Container name value
+#     container_name = "sample-task-two"
 #   }
 
 #   tags = {
 #     Name = "sample-task-two-service"
 #     host-header = "sample-task-two.cotb.dev"
+#     port = "80"
+#   }
+# }
+
+# resource "aws_ecs_task_definition" "sample_task_three" {
+#   # A unique name for your task definition
+#   family = "sample-task-three"
+
+#   requires_compatibilities  = ["EC2"]
+#   # Docker networking mode to use for the containers in the task.
+#   # network_mode              = "awsvpc"
+  
+#   # A list of valid container definitions provided as a single valid JSON document
+#   container_definitions = jsonencode([
+#     {
+#       name      = "sample-task-three"
+#       image     = "nginx:latest"
+#       cpu       = 128
+#       memory    = 256
+#       essential = true
+#       portMappings = [
+#         {
+#           containerPort = 8082
+#           hostPort = 8082
+#         }
+#       ]
+#       environment = [
+#         {
+#           "name" = "NGINX_PORT"
+#           "value" = "8082"
+#         }
+#       ]
+#       healthCheck = {
+#         command = [
+#           "CMD-SHELL",
+#           "curl -f http://localhost:80 || exit 1"
+#         ]
+#         interval = 10
+#         timeout = 2
+#         retries = 2
+#         startPeriod = 10
+#       }
+#     }
+#   ])
+# }
+
+# resource "aws_ecs_service" "sample_task_three_service" {
+#   # Name of the service.
+#   name            = "sample-task-three-service"
+#   # ARN of an ECS cluster.
+#   cluster         = aws_ecs_cluster.cotb_dev_cluster.id
+#   # Family and revision or full ARN of the task definition that you want to run in your service.
+#   task_definition = aws_ecs_task_definition.sample_task_three.arn
+#   # Number of instances of the task definition to place and keep running.
+#   desired_count   = 2
+#   # Launch type on which to run your service.
+#   launch_type     = "EC2"
+
+#   # Network configuration for the service. 
+#   # network_configuration {
+#   #   # Subnets associated with the task or service.
+#   #   subnets = [for subnet in aws_subnet.cotb_cluster_public_subnets : subnet.id]
+#   #   # Security groups associated with the task or service.
+#   #   security_groups    = [aws_security_group.cotb_cluster_public_sg.id]
+#   #   # Assign a public IP address to the ENI.
+#   #   assign_public_ip = false
+#   # }
+
+#   # Service discovery registries for the service. 
+#   service_registries {
+#     # ARN of the Service Registry.
+#     registry_arn = aws_service_discovery_service.cotb_cluster_sd_service_three.arn
+#     # Port value
+#     container_port = 8082
+#     # Container name value
+#     container_name = "sample-task-three"
+#   }
+
+#   tags = {
+#     Name = "sample-task-three-service"
+#     host-header = "sample-task-three.cotb.dev"
 #     port = "80"
 #   }
 # }
